b62013d3238536048d6013881c1bf41df900f6aa
Lenovo <Lenovo@LAPTOP-AKMSSU5M>
2022-11-09 02:23:41 -0800

    first commit

---
+++ src/javaapplication20/JavaApplication20.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication20;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class JavaApplication20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       public static void main(String[] args) {
+       int A = 7, B = 8, C = 4;
+       int Hasil = A*B*C;
+       System.out.print(Hasil);
+    }
+    
+}
